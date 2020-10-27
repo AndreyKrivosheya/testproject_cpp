9c0531d95b4ff3c3cc0643c9ef38c76b0b550af6
Andrey Krivosheya <rtti@bk.ru>
2020-10-28 00:54:56 +0300

    cleaned

--- bits/get_min_and_max_with_same_amount_of_bits_set.cpp
+++ bits/get_min_and_max_with_same_amount_of_bits_set.cpp
@@ -9,31 +9,16 @@ namespace bits
 {
     pair<unsigned int, unsigned int> get_min_and_max_with_same_amount_of_bits_set(const unsigned int& number)
     {
-        if(number == 0)
+        auto min = 0u, max = 0u;
+        for(auto numberTemp = number; numberTemp != 0; numberTemp = numberTemp >> 1)
         {
-            return make_pair(0u, 0u);
-        }
-        else
-        {
-            const unsigned int maxUnsignedInt = ((unsigned int)-1);
-            if(number == maxUnsignedInt)
+            if((numberTemp & 1u) == 1u)
             {
-                return make_pair(maxUnsignedInt, maxUnsignedInt);
+                min = (min << 1) | 1;
+                max = (max >> 1) | 0x80000000;
             }
-            else
-            {
-                auto min = 0u, max = 0u;
-                for(auto numberTemp = number; numberTemp != 0; numberTemp = numberTemp >> 1)
-                {
-                    if((numberTemp & 1u) == 1u)
-                    {
-                        min = (min << 1) | 1;
-                        max = (max >> 1) | 0x80000000;
-                    }
-                }
+        }
 
-                return make_pair(min, max);
-            }
-        }
+        return make_pair(min, max);
     }
 }
